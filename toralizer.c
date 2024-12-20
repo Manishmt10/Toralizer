25b4b106bd24cef69ed60d705ccfa5a3b6a6ae13
Manish <mt11497@gmail.com>
2024-12-21 03:07:17 +0530

    added basic TCP connection setup

--- toralizer.c
+++ toralizer.c
@@ -2,6 +2,45 @@
 
 #include "toralizer.h"
 
-int main(int *argc, char *argv[]) {
+int main(int argc, char *argv[]) {
+   char *host;
+   int port, sock_fd, cd;
+   struct sockaddr_in sock;
+
+   if(argc<3) {
+      fprintf(stderr,"Usage : %s <host> <port>\n",argv[0]);
+      return -1;
+   }
    
+   host = argv[1];
+   port = atoi(argv[2]);
+
+   sock_fd = socket(AF_INET,SOCK_STREAM,0);
+
+   if(sock_fd < 0) {
+      perror("Socket");
+      close(sock_fd);
+      return -1;
+   }
+
+   sock.sin_family = AF_INET;
+   sock.sin_port = htons(port);
+   sock.sin_addr.s_addr = inet_addr(host);
+
+   if(sock.sin_addr.s_addr == INADDR_NONE) {
+      fprintf(stderr,"Invalid IP Address : %s,\n",host);
+      return -1;
+   }
+
+   cd = connect(sock_fd,(struct sockaddr *)&sock, sizeof(sock));
+
+   if(cd < 0) {
+      perror("connection to proxy server failed");
+      close(sock_fd);
+      return -1;
+   }
+
+   printf("connected to proxy at %s:%d\n",host,port);
+   close(sock_fd);
+   return 0;
 }
--- toralizer.h
+++ toralizer.h
@@ -4,23 +4,28 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 #define PROXY "127.0.0.1"
 #define PROXY_PORT 9050
+#define reqsize sizeof(struct proxy_request)
+#define ressize sizeof(struct proxy_response)
 
 typedef unsigned char int8;
 typedef unsigned short int int16;
 typedef unsigned int int32;
 
 struct proxy_request {
-      int8 vn;
-      int8 cd;
-      int16 dstport;
-      int32 dstip;
-      unsigned char userid[8];
+   int8 vn;
+   int8 cd;
+   int16 dstport;
+   int32 dstip;
+   unsigned char userid[8];
 };
 
-typedef struct proxy_request req;
+typedef struct proxy_request Req;
 
 struct proxy_response {
    int8 vn;
@@ -29,6 +34,7 @@ struct proxy_response {
    int32 __;
 };
 
-typedef struct proxy_response res;
+typedef struct proxy_response Res;
 
-
+Req *request(const char *, const int);
+int main(int,char**);
